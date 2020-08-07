5f989160e227fc4e106abaf96375fbc7d21681f7
mr-winner <developer.winner@gmail.com>
2020-08-07 00:56:46 -0300

    fix description

---
+++ precache-manifest.1433ebbc37df3b65f1a6be1071fd17d6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b0a1c4760c1bc6b4af8651639989b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5044682c556712044de",
+    "url": "/static/css/main.b3df67f0.chunk.css"
+  },
+  {
+    "revision": "62c66ce4eca425242fa2",
+    "url": "/static/js/2.bcf81546.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bcf81546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5044682c556712044de",
+    "url": "/static/js/main.953e9ab7.chunk.js"
+  },
+  {
+    "revision": "11934dbdbbe1238f995e",
+    "url": "/static/js/runtime-main.97f1e9d9.js"
+  },
+  {
+    "revision": "948114af9fe60735a6de25fa281c190c",
+    "url": "/static/media/KOMIKAX_.948114af.ttf"
+  },
+  {
+    "revision": "b568750560bdcf6a450f45862bf82a56",
+    "url": "/static/media/coolvetica compressed rg.b5687505.ttf"
+  },
+  {
+    "revision": "518e553429fc2cbb8c1a4433c8609982",
+    "url": "/static/media/wallpoet.regular.518e5534.ttf"
+  }
+]);
